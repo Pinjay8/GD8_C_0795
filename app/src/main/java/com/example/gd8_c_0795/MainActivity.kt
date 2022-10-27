bd67adbba564ac18e620980bf0b96fba0945416e
Ace <alvincennanda@gmail.com>
2022-10-27 14:23:00 +0700

    Com

--- app/src/main/java/com/example/gd8_c_0795/MainActivity.kt
+++ app/src/main/java/com/example/gd8_c_0795/MainActivity.kt
@@ -18,109 +18,105 @@ import java.io.IOException
 import java.nio.charset.StandardCharsets
 
 class MainActivity : AppCompatActivity() {
-        var modelMainList:MutableList<ModelMain> = ArrayList()
-        lateinit var mapController: MapController
-        lateinit var overlayItem: ArrayList<OverlayItem>
+    var modelMainList: MutableList<ModelMain> = ArrayList()
+    lateinit var mapController:MapController
+    lateinit var overlayItem:ArrayList<OverlayItem>
 
-        override fun onCreate(savedInstanceState: Bundle?) {
-            super.onCreate(savedInstanceState)
-            setContentView(R.layout.activity_main)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        Configuration.getInstance().load(this, PreferenceManager.getDefaultSharedPreferences(this))
 
-            Configuration.getInstance().load(this, PreferenceManager.getDefaultSharedPreferences(this))
+        val geoPoint = GeoPoint(-7.78165, 110.414497)
+        mapView.setMultiTouchControls(true)
+        mapView.controller.animateTo(geoPoint)
+        mapView.setTileSource(TileSourceFactory.DEFAULT_TILE_SOURCE)
+        mapView.zoomController.setVisibility(CustomZoomButtonsController.Visibility.NEVER)
 
-            val geoPoint = GeoPoint(-7.78165,110.414497)
-            mapView.setMultiTouchControls(true)
-            mapView.controller.animateTo(geoPoint)
-            mapView.setTileSource(TileSourceFactory.DEFAULT_TILE_SOURCE)
-            mapView.zoomController.setVisibility(CustomZoomButtonsController.Visibility.NEVER)
+        mapController = mapView.controller as MapController
+        mapController.setCenter(geoPoint)
+        mapController.zoomTo(15)
 
-            mapController = mapView.controller as MapController
-            mapController.setCenter(geoPoint)
-            mapController.zoomTo(15)
+        getLocationMarker()
+    }
 
-            getLocationMarker()
-        }
-
-
-        private fun getLocationMarker(){
+    private fun getLocationMarker(){
+        try{
+            val stream = assets.open("sample_maps.json")
+            val size = stream.available()
+            val buffer = ByteArray(size)
+            stream.read(buffer)
+            stream.close()
+            val strContent = String(buffer, StandardCharsets.UTF_8)
             try{
-                val stream = assets.open("sample_maps.json")
-                val size = stream.available()
-                val buffer = ByteArray(size)
-                stream.read(buffer)
-                stream.close()
-                val strContent = String(buffer, StandardCharsets.UTF_8)
-                try{
-                    val jsonObject = JSONObject(strContent)
-                    val jsonArrayResult = jsonObject.getJSONArray("results")
-                    for(i in 0 until jsonArrayResult.length()) {
-                        val jsonObjectResult = jsonArrayResult.getJSONObject(i)
-                        val modelMain = ModelMain()
-                        modelMain.strName = jsonObjectResult.getString("name")
-                        modelMain.strVicinity = jsonObjectResult.getString("vicinity")
+                val jsonObject = JSONObject(strContent)
+                val jsonArrayResult = jsonObject.getJSONArray("results")
+                for(i in 0 until jsonArrayResult.length()) {
+                    val jsonObjectResult = jsonArrayResult.getJSONObject(i)
+                    val modelMain = ModelMain()
+                    modelMain.strName = jsonObjectResult.getString("name")
+                    modelMain.strVicinity = jsonObjectResult.getString("vicinity")
 
-
-                        val jsonObjectGeo = jsonObjectResult.getJSONObject("geometry")
-                        val jsonObjectLoc = jsonObjectGeo.getJSONObject("Location")
-                        modelMain.latLoc = jsonObjectLoc.getDouble("lat")
-                        modelMain.longLoc = jsonObjectLoc.getDouble("Lng")
-                        modelMainList.add(modelMain)
-                    }
-                    initMarker(modelMainList)
-                }catch (e: JSONException) {
-                    e.printStackTrace()
+                    val jsonObjectGeo = jsonObjectResult.getJSONObject("geometry")
+                    val jsonObjectLoc = jsonObjectGeo.getJSONObject("location")
+                    modelMain.latLoc = jsonObjectLoc.getDouble("lat")
+                    modelMain.longLoc = jsonObjectLoc.getDouble("lng")
+                    modelMainList.add(modelMain)
                 }
-            } catch (ignored: IOException) {
-                Toast.makeText(
-                    this@MainActivity,
-                    "Oops, ada yang tidak beres. Coba ulangi beberapa saat lagi.",
-                    Toast.LENGTH_SHORT
-                ).show()
+                initMarker(modelMainList)
+            }catch (e:JSONException) {
+                e.printStackTrace()
             }
+        }catch (ignored:IOException) {
+            Toast.makeText(
+                this@MainActivity,
+                "Oops, ada yang tidak beres. Coba ulangi beberapa saat lagi.",
+                Toast.LENGTH_SHORT
+            ).show()
         }
+    }
 
-        private fun  initMarker(modelList: List<ModelMain>) {
-            for (i in modelList.indices) {
-                overlayItem = ArrayList()
-                overlayItem.add(
-                    OverlayItem(
-                        modelList[i].strName, modelList[i].strVicinity, GeoPoint(
-                            modelList[i].latLoc, modelList[i].longLoc
-                        )
+    private fun initMarker(modelList: List<ModelMain>) {
+        for(i in modelList.indices) {
+            overlayItem = ArrayList()
+            overlayItem.add(
+                OverlayItem(
+                    modelList[i].strName, modelList[i].strVicinity, GeoPoint(
+                        modelList[i].latLoc, modelList[i].longLoc
                     )
                 )
-                val info = ModelMain()
-                info.strName = modelList[i].strName
-                info.strVicinity = modelList[i].strVicinity
+            )
+            val info = ModelMain()
+            info.strName = modelList[i].strName
+            info.strVicinity = modelList[i].strVicinity
 
-                val marker = Marker(mapView)
-                marker.icon = resources.getDrawable(R.drawable.ic_baseline_location_on_24)
-                marker.position = GeoPoint(modelList[i].latLoc, modelList[i].longLoc)
-                marker.relatedObject = info
-                marker.infoWindow = CustomInfoWindow(mapView)
-                marker.setOnMarkerClickListener { item, arg1 ->
-                    item.showInfoWindow()
-                    true
-                }
-
-                mapView.overlays.add(marker)
-                mapView.invalidate()
+            val marker = Marker(mapView)
+            marker.icon = resources.getDrawable(R.drawable.ic_baseline_location_on_24)
+            marker.position = GeoPoint(modelList[i].latLoc, modelList[i].longLoc)
+            marker.relatedObject = info
+            marker.infoWindow = CustomInfoWindow(mapView)
+            marker.setOnMarkerClickListener { item, arg1->
+                item.showInfoWindow()
+                true
             }
+            mapView.overlays.add(marker)
+            mapView.invalidate()
         }
+    }
 
-        public override fun onResume() {
-            super.onResume()
-            Configuration.getInstance().load(this, PreferenceManager.getDefaultSharedPreferences(this))
-            if (mapView != null){
-                mapView.onResume()
-            }
+    public override fun onResume() {
+        super.onResume()
+        Configuration.getInstance().load( this, PreferenceManager.getDefaultSharedPreferences(this))
+        if(mapView != null) {
+            mapView.onResume()
         }
+    }
 
-        public override fun onPause() {
-            super.onPause()
-            Configuration.getInstance().load(this, PreferenceManager.getDefaultSharedPreferences(this))
-            if(mapView != null){
-                mapView.onPause()
-            }
+    public override fun onPause() {
+        super.onPause()
+        Configuration.getInstance().load( this, PreferenceManager.getDefaultSharedPreferences(this))
+        if(mapView != null) {
+            mapView.onPause()
         }
+    }
 }
